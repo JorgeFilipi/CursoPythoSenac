85e1f19c399cf5bc7f9814f376a785b36e65238e
JorgeFilipi <160806240+JorgeFilipi@users.noreply.github.com>
2024-09-25 23:00:24 -0300

    primeiro commit PyCharm

---
+++ UC3/Aula1a.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número para verificar se é par ou impar: "))
+
+if num % 2 == 0:
+    print("par")
+else:
+    print("impar")
---
+++ UC3/Aula1b.py
@@ -0,0 +1,6 @@
+idade = int(input("Qual é sua idade? "))
+
+if idade >= 16:
+    print("já pode votar")
+else:
+    print("ainda não pode votar")
