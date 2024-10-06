50645bf67c52293cb32a10375eecf06ba74296b1
JorgeFilipi <160806240+JorgeFilipi@users.noreply.github.com>
2024-10-05 21:08:57 -0300

    Aula5

--- UC3/Aula5/Atividade/app.py
+++ UC3/Aula5/Atividade/app.py
@@ -2,28 +2,28 @@ from flask import Flask, render_template, url_for, redirect, request
 
 app=Flask(__name__)
 
-tarefas = []
+bikes = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", tarefas=tarefas)
+    return render_template("index.html", bikes=bikes)
 
 @app.route("/adicionar", methods=["POST"])
 def adicionar():
-    nova_tarefa = request.form.get("tarefa")
-    if nova_tarefa:
-        tarefa = {"conteudo": nova_tarefa, "completa": False}
-        tarefas.append(tarefa)
+    nova_bike = request.form.get("bike")
+    if nova_bike:
+        nova_bike = {"marca": nova_bike, "status": False}
+        bikes.append(nova_bike)
     return redirect(url_for("index"))
 
-@app.route("/completar/<int:tarefa_id>")
-def completar(tarefa_id):
-    tarefas[tarefa_id]["completa"] = True
+@app.route("/completar/<int:bike_id>")
+def completar(bike_id):
+    bikes[bike_id]["status"] = True
     return redirect(url_for("index"))
 
-@app.route("/remover/<int:tarefa_id>")
-def remover(tarefa_id):
-    tarefas.pop(tarefa_id)
+@app.route("/remover/<int:bike_id>")
+def remover(bike_id):
+    bikes.pop(bike_id)
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
