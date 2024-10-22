0cdab640059d72b4712ace41ddab5a625a022480
JorgeFilipi <160806240+JorgeFilipi@users.noreply.github.com>
2024-10-22 11:37:54 -0300

    Delete UC3 directory
    Delete UC3 directory

--- UC3/Aula1a.py
+++
@@ -1,6 +0,0 @@
-num = int(input('Digite um número para verifier se é par ou impar: '))
-
-if num % 2 == 0:
-    print("par")
-else:
-    print("impar")
--- UC3/Aula1b.py
+++
@@ -1,6 +0,0 @@
-idade = int(input("Qual é sua idade? "))
-
-if idade >= 16:
-    print("já pode votar")
-else:
-    print("ainda não pode votar")
